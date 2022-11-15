faa536a7152c8a0a5f15316deed7ac497218f890
carlosjssc <carlos.clara@univale.br>
2022-11-15 08:51:26 -0300

    Atualização teste concatenação

---
+++ trilhas-java-basico/operadores/src/AulaOperadores.java
@@ -0,0 +1,21 @@
+public class AulaOperadores 
+{
+    public static void main(String[] args) 
+    {
+        String nomeCompleto = "Linguagem " + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
